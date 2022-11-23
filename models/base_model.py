f2812f0c12ff55754db316d7e0222519e5e78e2d
Selehadin <selehadinseid1@gmail.com>
2022-11-23 11:55:35 +0000

    add fix

--- models/base_model.py
+++ models/base_model.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
 from datetime import date
 
+
 class BaseModel():
     """
     Parent class that defines all common attributes and methods
     of hbnb project
     """
 
-
-    def __init__(self, created_at, updated_at):
+    def __init__(self, *ags, **kwargs):
         """
         Instantiating puplic instance attributes
         """
-
-        self.id = uuid.uuid4()
-        self.created_at = created_at
-        self.updated_at = updated_at
+        if kwargs:
+            for name, attr in kwargs.items():
+                if name != '__class__':
+                    if name not in ['created_at', 'updated_at']:
+                        self.__setattr__(name, attr)
+                    else:
+                        self.__setattr__(name, datetime.fromisoformat(attr))
+        else:
+            self.id = str(uid.uuid4())
+            self.created_at = datetime.noe()
+            self.updated_at = datetime.now()
 
     def __str__:
         """
@@ -24,11 +31,11 @@ class BaseModel():
 
     def save(self):
         """
-        updates the public instance attribute updated_at with the 
+        updates the public instance attribute updated_at with the
         current datetime.
         """
 
-        self.updated_at = date.now.isoformat();
+        self.updated_at = date.now.isoformat()
 
     def to_dict(self):
         """
@@ -36,4 +43,14 @@ class BaseModel():
         __dict__ of the instance.
         """
 
-        return (class.self.__dict__)
+        to_dict = {}
+        for key, value in self.__dict__.items():
+            to_dict[key] = value
+
+        to_dict['__class__'] = self.__class__.__name__
+        to_dict['created_at'] = self.created_at.\
+                strftime("%Y-%m-%dT%H:%M:%S.%f")
+        to_dict['updated_at'] = self.updated_at.\
+                strftime("%Y-%m-%dT%H:%M:%S.%f")
+
+        return (to_dict)
